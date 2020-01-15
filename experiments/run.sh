8dbb02e0cd5a26e2e7d2b03d7e13f8f366469d6a
Marco Ancona <marco.ancona@inf.ethz.ch>
2020-01-15 21:18:25 +0100

    included notebook to display results

--- experiments/run.sh
+++ experiments/run.sh
@@ -5,14 +5,14 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)/.."
 
 
 # Dynamic pruning ratio
-for i in {1..3}; do python3 train.py --experiment fmnist --sparsity 0 --pruning-steps 45 --pruning-logic sv-zeros; done
-for i in {1..3}; do python3 train.py --experiment fmnist --sparsity 0 --pruning-steps 45 --pruning-logic sv-nonpositive; done
+for i in {1..3}; do python3 train.py --experiment fmnist --sparsity 0 --pruning-steps 30 --pruning-logic sv-zeros; done
+for i in {1..3}; do python3 train.py --experiment fmnist --sparsity 0 --pruning-steps 30 --pruning-logic sv-nonpositive; done
 
 # Fixed pruning ratio
-for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 45 --pruning-logic grad-abs-smallest; done
-for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 45 --pruning-logic count-smallest; done
-for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 45 --pruning-logic sv-abs-smallest; done
-for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 45 --pruning-logic random; done
+for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 30 --pruning-logic grad-abs-smallest; done
+for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 30 --pruning-logic count-smallest; done
+for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 30 --pruning-logic sv-abs-smallest; done
+for i in {1..3}; do python3 train.py --sparsity 0.15 --pruning-steps 30 --pruning-logic random; done
 
 # Non pruning
 for i in {1..2}; do python3 fmnist_train.py --sparsity 1 --pruning-steps 0 --pruning-logic random; done
